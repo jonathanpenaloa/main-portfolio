038c54b89acaddeff2df5db7fc95b9db45063699
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-10 11:44:32 -0800

    hero section position using css

--- src/components/HeroImg.jsx
+++ src/components/HeroImg.jsx
@@ -1,10 +1,22 @@
 import "./HeroImg.css";
-
+import React from "react";
+import BackGroundImg from "../assets/background.jpg";
+import { Link } from "react-router-dom";
 export default function HereImg() {
     return (
         <div className="hero">
             <div className="mask">
-                
+                <img className="background-img" src={BackGroundImg} alt="BackGroundimg" />
+            </div>
+            <div className="content">
+                <p>Hi, IM working</p>
+                <h1>React Developer</h1>
+                <div>
+                    <Link to="/project" className="btn">Projects
+                    </Link>
+                    <Link to="/contant" className="btn btn-light">Contact
+                    </Link>
+                </div>
             </div>
         </div>
     )
