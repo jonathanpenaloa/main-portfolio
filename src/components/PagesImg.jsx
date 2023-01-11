90d3fbbccb13cb04b898087518166734c8c080cf
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-10 16:33:18 -0800

    css added for pagesImg

---
+++ src/components/PagesImg.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./PagesImg.css"
+
+export default function PagesImg () {
+    return (
+        <div className="pages-img">
+            <div className="heading">
+                <h1>This is headeing</h1>
+                <p>This is text</p>
+            </div>
+        </div>
+    ) 
+};
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import PagesImg from "../components/PagesImg";
 
 export default function Projects() {
     return (
         <div>
             <NavBar/>
+            <PagesImg />
             <Footer/>
         </div>
     );
