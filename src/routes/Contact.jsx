ed87438c316eba94c3ffa7d5c16480945582dedb
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-10 17:03:06 -0800

    msg page added to comps

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import PagesImg from "../components/PagesImg";
 
 
 export default function Contact() {
     return (
         <div>
             <NavBar/>
+            <PagesImg heading="Contact" text="Lets talk"/>
             <Footer/>
         </div>
     );
