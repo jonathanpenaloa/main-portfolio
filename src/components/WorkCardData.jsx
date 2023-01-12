c5d9d3d168cb69dd6bd5813e5997743c00446044
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-12 13:36:54 -0800

    assets renamed jpg

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -1,7 +1,7 @@
-import project1 from "../assets/project1.png";
-import project2 from "../assets/project2.png";
-import project3 from "../assets/project3.png";
-import project4 from "../assets/project4.png";
+import project1 from "../assets/project1.jpg";
+import project2 from "../assets/project2.jpg";
+import project3 from "../assets/project3.jpg";
+import project4 from "../assets/project4.jpg";
 
 const ProjectCardData = [
     {
