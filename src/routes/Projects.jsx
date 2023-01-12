02bfab5391bd7dfaa63780bde716a9ffaa5134c2
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-11 18:31:28 -0800

    workcard comp added styled

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import PagesImg from "../components/PagesImg";
+import WorkCard from "../components/WorkCard";
 
 export default function Projects() {
     return (
         <div>
             <NavBar/>
             <PagesImg heading="PROJECTS." text="Some text" />
+            <WorkCard />
             <Footer/>
         </div>
     );
