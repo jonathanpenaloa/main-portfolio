d09533352a7d17ff7e72277a962f894f27d02ff3
jonathanpenaloa <jona_pena_91@outlook.com>
2023-01-13 18:01:26 -0800

    project comp showing in home comp

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import HeroImg from "../components/HeroImg";
 import Footer from "../components/Footer";
+import Work from "../components/Work";
 
 export default function Home() {
     return (
         <>
             <NavBar/>
             <HeroImg />
+            <Work />
             <Footer />
         </>
     );
